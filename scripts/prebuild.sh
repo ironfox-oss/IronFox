c4fdcac76e2614daf82ee509fe06651b9f4ed7df
celenity <celenity@celenity.dev>
2025-01-14 22:39:31 +0000

    feat: Set buildtime options to ensure we disable telemetry & normandy...
    Signed-off-by: celenity <celenity@celenity.dev>

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -360,6 +360,9 @@ patch -p1 --no-backup-if-mismatch --quiet <"$patches/remove-default-sites.patch"
 # By default, Fenix includes 2 hidden web extensions, used exclusively for telemetry: 'ads@mozac.org' & 'cookies@mozac.org' - This prevents installing them...
 patch -p1 --no-backup-if-mismatch --quiet <"$patches/do-not-install-telemetry-extensions.patch"
 
+# Ensure we're disabling telemetry at buildtime...
+patch -p1 --no-backup-if-mismatch --quiet <"$patches/buildtime-disable-telemetry.patch"
+
 # Fix v125 compile error
 patch -p1 --no-backup-if-mismatch --quiet <"$patches/gecko-fix-125-compile.patch"
 
