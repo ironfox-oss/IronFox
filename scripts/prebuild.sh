74918d045521debaaa9466b21a7da5b2291dd188
celenity <celenity@celenity.dev>
2025-01-22 02:55:59 +0000

    fix: Update to Phoenix 2025.01.22.1 - https://codeberg.org/celenity/Phoenix/releases/tag/2025.01.22.1
    Signed-off-by: celenity <celenity@celenity.dev>

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -457,8 +457,7 @@ pref("media.gmp-manager.url.override", "data:text/plain,");
 pref("media.gmp-gmpopenh264.enabled", false);
 EOF
 
-cat "$patches/preferences/phoenix.js" >>mobile/android/app/geckoview-prefs.js
-cat "$patches/preferences/phoenix-extended.js" >>mobile/android/app/geckoview-prefs.js
+cat "$patches/preferences/phoenix-extended-android.js" >>mobile/android/app/geckoview-prefs.js
 cat "$patches/preferences/ironfox.js" >>mobile/android/app/geckoview-prefs.js
 
 popd
