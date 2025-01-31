3e3bb2a6ba550d1f5b59d119c9f6efa773798f8e
Akash Yadav <itsaky01@gmail.com>
2025-01-31 22:38:05 +0530

    fix: remove unnecessary patch command

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -282,9 +282,6 @@ patch -p1 --no-backup-if-mismatch --quiet <"$patches/fenix-liberate.patch"
 # Support spoofing locale to 'en-US'
 patch -p1 --no-backup-if-mismatch --quiet <"$patches/tor-spoof-english.patch"
 
-# Add a toggle in settings for spoofing locale to 'en-US'
-patch -p1 --no-backup-if-mismatch --quiet <"$patches/tor-spoof-english-switch.patch"
-
 # Set strict ETP by default
 patch -p1 --no-backup-if-mismatch --quiet <"$patches/strict_etp.patch"
 
