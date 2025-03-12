556559c4d06c0c1a7abb16fa8d6ab475fb61c911
Akash Yadav <itsaky01@gmail.com>
2025-03-12 11:43:55 +0530

    fix: always use current FIREFOX_TAG for android-components version in application-services

--- scripts/get_sources.sh
+++ scripts/get_sources.sh
@@ -2,28 +2,7 @@
 
 set -euo pipefail
 
-FIREFOX_TAG="136.0.1"
-FIREFOX_RELEASE_PATH="releases/${FIREFOX_TAG}"
-# FIREFOX_RC_BUILD_NAME="build2"
-# FIREFOX_RELEASE_PATH="candidates/${FIREFOX_TAG}-candidates/${FIREFOX_RC_BUILD_NAME}"
-WASI_TAG="wasi-sdk-20"
-GLEAN_TAG="v63.0.0"
-GMSCORE_TAG="v0.3.6.244735"
-APPSERVICES_TAG="release-v136"
-BUNDLETOOL_TAG="1.18.0"
-
-# Configuration
-ROOTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
-ENV_SH="${ROOTDIR}/scripts/env_local.sh"
-BUILDDIR="${ROOTDIR}/build"
-PATCHDIR="${ROOTDIR}/patches"
-GECKODIR="${ROOTDIR}/gecko"
-GLEANDIR="${ROOTDIR}/glean"
-APPSERVICESDIR="${ROOTDIR}/appservices"
-GMSCOREDIR="${ROOTDIR}/gmscore"
-WASISDKDIR="${ROOTDIR}/wasi-sdk"
-ANDROID_COMPONENTS="${GECKODIR}/mobile/android/android-components"
-FENIX="${GECKODIR}/mobile/android/fenix"
+source "$(dirname $0)/versions.sh"
 
 clone_repo() {
     url="$1"
