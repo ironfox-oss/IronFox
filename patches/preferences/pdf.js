059cb6de704c103237557a45efdae8f59cb266e8
celenity <celenity@celenity.dev>
2025-04-14 17:54:02 -0400

    feat: Set PDF.js prefs via `PdfJsOverridePrefs.js` instead of using a patch
    Signed-off-by: celenity <celenity@celenity.dev>

---
+++ patches/preferences/pdf.js
@@ -0,0 +1,9 @@
+//
+// See '019 PDF.js' in Phoenix for more details
+
+pref("pdfjs.enablePermissions", false);
+pref("pdfjs.enableScripting", false);
+pref("pdfjs.enableSignatureEditor", true);
+pref("pdfjs.enableXfa", false);
+pref("pdfjs.externalLinkTarget", 2);
+pref("pdfjs.sidebarViewOnLoad", 2);
