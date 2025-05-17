ec8709b067240ca70017426cd27e23eba625fdaa
Akash Yadav <itsaky01@gmail.com>
2025-05-18 02:20:41 +0530

    fix: override fenix build date using IF_BUILD_DATE
    Signed-off-by: Akash Yadav <itsaky01@gmail.com>

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -66,6 +66,7 @@ fi
 # Set the build date to the date of commmit to ensure that the
 # MOZ_BUILDID is consistent across CI build jobs
 export MOZ_BUILD_DATE="$(date -d "$CI_PIPELINE_CREATED_AT" "+%Y%m%d%H%M%S")"
+export IF_BUILD_DATE="$(($(date -u -d "$CI_PIPELINE_CREATED_AT" +%s) * 1000))"
 
 # Build
 bash -x scripts/build.sh "$BUILD_TYPE"
