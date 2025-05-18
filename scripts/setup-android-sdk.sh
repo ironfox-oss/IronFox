9b410eabdbf673964ae091a5f160d789f97dac83
celenity <celenity@celenity.dev>
2025-05-17 20:58:14 -0400

    Add (WIP) macOS build support
    Signed-off-by: celenity <celenity@celenity.dev>

--- scripts/setup-android-sdk.sh
+++ scripts/setup-android-sdk.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 SDK_REVISION=13114758
-ANDROID_SDK_FILE=commandlinetools-linux-${SDK_REVISION}_latest.zip
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    ANDROID_SDK_FILE=commandlinetools-mac-${SDK_REVISION}_latest.zip
+else
+    ANDROID_SDK_FILE=commandlinetools-linux-${SDK_REVISION}_latest.zip
+fi
 
 if [[ "${ANDROID_HOME+x}" == "" ]]; then
     export ANDROID_HOME=$HOME/android-sdk
@@ -43,7 +48,16 @@ export PATH
 
 $SDK_MANAGER 'build-tools;35.0.1' # for GeckoView
 $SDK_MANAGER 'ndk;26.2.11394342'  # for GleanAS
-$SDK_MANAGER 'ndk;28.0.13004108'  # for mozbuild; application-services
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "Downloading Android NDK..."
+    wget https://dl.google.com/android/repository/android-ndk-r28-darwin.zip
+    unzip -q android-ndk-r28-darwin.zip -d "$ANDROID_HOME/ndk"
+    mv "$ANDROID_HOME/ndk/android-ndk-r28" "$ANDROID_HOME/ndk/28.0.13004108"
+    rm -vf android-ndk-r28-darwin.zip
+else
+    $SDK_MANAGER 'ndk;28.0.13004108'  # for mozbuild; application-services
+fi
 
 export ANDROID_NDK="$ANDROID_HOME/ndk/28.0.13004108"
 [ -d "$ANDROID_NDK" ] || {
