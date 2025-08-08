a1311b8dc605bf3ffa4fcb1b330dae3b8082098d
celenity <celenity@celenity.dev>
2025-08-08 16:06:54 +0000

    IronFox v141.0.3

--- patches/preferences/ironfox.js
+++ patches/preferences/ironfox.js
@@ -23,9 +23,17 @@ pref("privacy.resistFingerprinting.block_mozAddonManager", true);
 // Unlike standard Firefox, this doesn't actually enable the Beacon API; this just enables our stub - see the `stub-beacon` patch for more details
 pref("beacon.enabled", true); // [DEFAULT]
 
+/// Re-enable geolocation permission prompts in GeckoView
+// We still block this by default, just via a patch for Fenix's UI settings instead
+pref("geo.prompt.testing", false); // [HIDDEN] [DEFAULT]
+
 /// Re-enable media autoplay in GeckoView
 // We still block this by default, just via a patch for Fenix's UI settings instead
 pref("media.geckoview.autoplay.request.testing", 0); // [DEFAULT]
+
+/// Re-enable notification permission prompts in GeckoView
+// We still block this by default, just via a patch for Fenix's UI settings instead
+pref("notification.prompt.testing", false); // [HIDDEN] [DEFAULT]
 
 /// Re-enable Password Manager and Autofill in GeckoView
 // We still disable these by default, just via a patch for Fenix's UI settings instead
@@ -39,12 +47,8 @@ pref("signon.rememberSignons", true); // [DEFAULT]
 pref("webgl.disabled", false); // [DEFAULT]
 
 /// Restrict Remote Settings
-pref("browser.ironfox.services.settings.allowedCollections", "blocklists/addons,blocklists/addons-bloomfilters,blocklists/gfx,blocklists/plugins,main/addons-manager-settings,main/anti-tracking-url-decoration,main/bounce-tracking-protection-exceptions,main/fingerprinting-protection-overrides,main/hijack-blocklists,main/ml-onnx-runtime,main/partitioning-exempt-urls,main/password-recipes,main/query-stripping,main/remote-permissions,main/tracking-protection-lists,main/third-party-cookie-blocking-exempt-urls,main/translations-identification-models,main/translations-models,main/translations-wasm,main/url-classifier-exceptions,main/url-classifier-skip-urls,main/url-parser-default-unknown-schemes-interventions,security-state/cert-revocations,security-state/ct-logs,security-state/intermediates,security-state/onecrl");
+pref("browser.ironfox.services.settings.allowedCollections", "blocklists/addons,blocklists/addons-bloomfilters,blocklists/gfx,blocklists/plugins,main/addons-manager-settings,main/anti-tracking-url-decoration,main/bounce-tracking-protection-exceptions,main/cookie-banner-rules-list,main/fingerprinting-protection-overrides,main/hijack-blocklists,main/ml-onnx-runtime,main/partitioning-exempt-urls,main/password-recipes,main/query-stripping,main/remote-permissions,main/tracking-protection-lists,main/third-party-cookie-blocking-exempt-urls,main/translations-identification-models,main/translations-models,main/translations-wasm,main/url-classifier-exceptions,main/url-classifier-skip-urls,main/url-parser-default-unknown-schemes-interventions,security-state/cert-revocations,security-state/ct-logs,security-state/intermediates,security-state/onecrl");
 pref("browser.ironfox.services.settings.allowedCollectionsFromDump", "main/ironfox-fingerprinting-protection-overrides,blocklists/addons,blocklists/addons-bloomfilters,blocklists/gfx,main/anti-tracking-url-decoration,main/cookie-banner-rules-list,main/moz-essential-domain-fallbacks,main/password-recipes,main/remote-permissions,main/translations-models,main/translations-wasm,main/url-classifier-skip-urls,main/url-parser-default-unknown-schemes-interventions,security-state/intermediates,security-state/onecrl");
-
-/// Set default process count
-// This is the same as the default value - but we need to set here since Mozilla only defines it in GeckoRuntimeSettings, which we override to allow users to change the value if desired
-pref("dom.ipc.processCount", 2); // [DEFAULT]
 
 /// Set light/dark mode to match system
 // We still enable light mode by default, just via a patch for Fenix's UI settings instead
