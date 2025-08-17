13c0bda7ff3b914efb4d75c4418a5b787c43665f
celenity <celenity@celenity.dev>
2025-08-16 20:51:09 -0400

    fix: fix-up (+ minor tweaks/refinements)
    Signed-off-by: celenity <celenity@celenity.dev>

---
+++ patches/fenix-overlay/app/src/main/java/org/mozilla/fenix/components/GleanHelper.kt
@@ -0,0 +1,6 @@
+/* Hi, I'm a stub. ;) */
+
+package org.mozilla.fenix.components
+
+fun initializeGlean() {
+}
---
+++ patches/fenix-overlay/app/src/main/java/org/mozilla/fenix/components/metrics/GleanUsageReporting.kt
@@ -0,0 +1,24 @@
+/* Hi, I'm a stub. ;) */
+
+package org.mozilla.fenix.components.metrics
+
+class GleanUsageReporting : GleanUsageReportingApi {
+
+    override fun setEnabled(enabled: Boolean) {
+    }
+
+    override fun setDuration(durationMillis: Long) {
+    }
+
+    override fun setUsageReason(usageReason: GleanUsageReportingApi.UsageReason) {
+    }
+
+    override fun submitPing() {
+    }
+
+    override fun requestDataDeletion() {
+    }
+
+    private fun setUsageConstantValues() {
+    }
+}
