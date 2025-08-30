36c6a9c3d9f2e54e759eebdf766ed193835f1d95
celenity <celenity@celenity.dev>
2025-08-30 17:00:09 -0400

    fix: Minor clean-up for stubs
    Signed-off-by: celenity <celenity@celenity.dev>

--- patches/fenix-overlay/app/src/main/java/org/mozilla/fenix/components/metrics/MarketingAttributionService.kt
+++ patches/fenix-overlay/app/src/main/java/org/mozilla/fenix/components/metrics/MarketingAttributionService.kt
@@ -5,9 +5,7 @@ package org.mozilla.fenix.components.metrics
 import android.content.Context
 
 class MarketingAttributionService(private val context: Context) {
-    fun start() {
-    }
+    fun start() {}
 
-    fun stop() {
-    }
+    fun stop() {}
 }
