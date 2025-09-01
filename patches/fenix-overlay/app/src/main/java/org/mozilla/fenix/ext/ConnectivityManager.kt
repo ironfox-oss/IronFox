4a5af654d2a5c0b8f7b04e17b3581ae77d9f3797
celenity <celenity@celenity.dev>
2025-09-01 15:42:09 -0400

    feat: Stub ConnectivityManager
    Signed-off-by: celenity <celenity@celenity.dev>

---
+++ patches/fenix-overlay/app/src/main/java/org/mozilla/fenix/ext/ConnectivityManager.kt
@@ -0,0 +1,8 @@
+/* Hi, I'm a stub. ;) */
+
+package org.mozilla.fenix.ext
+
+import android.net.ConnectivityManager
+import android.net.Network
+
+fun ConnectivityManager.isOnline(network: Network? = null): Boolean = true
