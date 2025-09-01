c2e34d6f5bb806808fb390cfc01dcad4496495a4
celenity <celenity@celenity.dev>
2025-09-01 15:42:59 -0400

    fix: Remove certain libraries from Gradle versions + remove Crash Reporter from Push
    Signed-off-by: celenity <celenity@celenity.dev>

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -250,6 +250,8 @@ rm -vrf app/src/*/java/org/mozilla/fenix/components/metrics/fonts
 rm -vrf app/src/*/java/org/mozilla/fenix/settings/datachoices
 rm -vrf app/src/*/java/org/mozilla/fenix/telemetry
 
+sed -i -e 's|val push by lazyMonitored { Push(context, analytics.crashReporter) }|val push by lazyMonitored { Push(context) }|' app/src/main/java/org/mozilla/fenix/components/Components.kt
+
 # Let it be IronFox
 $SED -i \
     -e 's/Notifications help you stay safer with Firefox/Enable notifications/' \
