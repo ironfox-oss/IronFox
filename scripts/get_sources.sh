7d08b6a0ee985bee7a7111afb8e7d4019f64c8ad
celenity <celenity@celenity.dev>
2025-09-15 17:30:37 -0400

    fix: Ensure `wasi-sdk` directory exists for prebuilds
    Signed-off-by: celenity <celenity@celenity.dev>

--- scripts/get_sources.sh
+++ scripts/get_sources.sh
@@ -185,11 +185,13 @@ if [[ -n ${FDROID_BUILD+x} ]]; then
     clone_repo "https://github.com/WebAssembly/wasi-sdk" "$WASISDKDIR" "$WASI_TAG"
     (cd "$WASISDKDIR" && git submodule update --init --depth=64)
 elif [[ "$PLATFORM" == "macos" ]]; then
+    mkdir -vp wasi-sdk
     echo "Downloading prebuilt wasi-sdk..."
     download "https://github.com/celenityy/wasi-sdk/releases/download/$WASI_TAG/$WASI_TAG-firefox-osx.tar.xz" "$BUILDDIR/wasi-sdk.tar.xz"
     mkdir -vp "$WASISDKDIR"
     tar xJf "$BUILDDIR/wasi-sdk.tar.xz" -C "$WASISDKDIR"
 else
+    mkdir -vp wasi-sdk
     echo "Downloading prebuilt wasi-sdk..."
     download_and_extract "wasi-sdk" "https://github.com/itsaky/ironfox/releases/download/$WASI_TAG/$WASI_TAG-firefox.tar.xz"
 fi
