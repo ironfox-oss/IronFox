b6eeae8aa6a9240ec2a34b3149915806d0443800
Akash Yadav <itsaky01@gmail.com>
2025-10-25 21:09:39 +0530

    fix(builder): remove glean uniffi patch apply call
    Signed-off-by: Akash Yadav <itsaky01@gmail.com>

--- builder/src/steps/prepare/glean.py
+++ builder/src/steps/prepare/glean.py
@@ -144,12 +144,6 @@ rust.targets={current_platform().lower()}-{current_machine()},{config.rusttarget
             ],
         ),
         
-        d.patch(
-            name="Apply glean-noop-uniffi.patch",
-            patch_file=paths.patches_dir / "glean-noop-uniffi.patch",
-            target_dir=paths.glean_dir
-        ),
-        
         # Use Tor's no-op UniFFi binding generator
         *_process_file(
             path="glean-core/android/build.gradle",
