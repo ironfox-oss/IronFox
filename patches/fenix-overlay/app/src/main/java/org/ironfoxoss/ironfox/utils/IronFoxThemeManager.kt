3802c6a1da136e8c73f34477eb323156e7a82ca0
Akash Yadav <itsaky01@gmail.com>
2025-12-23 13:10:22 +0530

    fix: move OLED settings to fenix-overlay
    Signed-off-by: Akash Yadav <itsaky01@gmail.com>

--- patches/fenix-overlay/app/src/main/java/org/ironfoxoss/ironfox/utils/IronFoxThemeManager.kt
+++ patches/fenix-overlay/app/src/main/java/org/ironfoxoss/ironfox/utils/IronFoxThemeManager.kt
@@ -1,54 +1,12 @@
 package org.ironfoxoss.ironfox.utils
 
-import androidx.compose.foundation.isSystemInDarkTheme
+import android.content.Context
 import androidx.compose.material3.ColorScheme
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.ReadOnlyComposable
-import androidx.compose.ui.platform.LocalContext
 import mozilla.components.compose.base.theme.AcornColors
-import mozilla.components.compose.base.theme.AcornTheme
 import mozilla.components.compose.base.theme.acornDarkColorScheme
-import mozilla.components.compose.base.theme.acornLightColorScheme
-import mozilla.components.compose.base.theme.acornPrivateColorScheme
 import mozilla.components.compose.base.theme.darkColorPalette
-import mozilla.components.compose.base.theme.lightColorPalette
-import mozilla.components.compose.base.theme.privateColorPalette
 import mozilla.components.compose.base.theme.toM3ColorScheme
-import mozilla.components.compose.base.utils.inComposePreview
 import mozilla.components.ui.colors.PhotonColors
-import org.mozilla.fenix.ext.settings
-import org.mozilla.fenix.theme.Theme
-import org.mozilla.fenix.theme.Theme.Dark
-import org.mozilla.fenix.theme.Theme.Light
-import org.mozilla.fenix.theme.Theme.Oled
-import org.mozilla.fenix.theme.Theme.Private
-
-/**
- * @author Akash Yadav
- */
-object IronFoxThemeManager {
-
-	@Composable
-	@ReadOnlyComposable
-	fun getTheme(allowPrivateTheme: Boolean = true): Theme {
-		if (allowPrivateTheme &&
-			!inComposePreview &&
-			LocalContext.current.settings().lastKnownMode.isPrivate
-		) {
-			return Private
-		}
-
-		if (isSystemInDarkTheme()) {
-			if (LocalContext.current.settings().ironfox.shouldUseOledTheme) {
-				return Oled
-			}
-
-			return Dark
-		}
-
-		return Light
-	}
-}
 
 val oledColorPalette = darkColorPalette.copy(
 	layer1 = PhotonColors.Black,
@@ -83,28 +41,18 @@ fun acornOledColorScheme(): ColorScheme {
 	)
 }
 
-@Composable
-fun IronFoxTheme(
-	theme: Theme = IronFoxThemeManager.getTheme(),
-	content: @Composable () -> Unit,
-) {
-	val colors: AcornColors = when (theme) {
-		Light -> lightColorPalette
-		Dark -> darkColorPalette
-		Oled -> oledColorPalette
-		Private -> privateColorPalette
+fun AcornColors.maybeApplyOledColors(context: Context): AcornColors {
+	if (IronFoxPreferences.shouldUseOledTheme(context)) {
+		return oledColorPalette
 	}
 
-	val colorScheme: ColorScheme = when (theme) {
-		Light -> acornLightColorScheme()
-		Dark -> acornDarkColorScheme()
-		Oled -> acornOledColorScheme()
-		Private -> acornPrivateColorScheme()
+	return this
+}
+
+fun ColorScheme.maybeApplyOledColors(context: Context): ColorScheme {
+	if (IronFoxPreferences.shouldUseOledTheme(context)) {
+		return acornOledColorScheme()
 	}
 
-	AcornTheme(
-		colors = colors,
-		colorScheme = colorScheme,
-		content = content,
-	)
+	return this
 }
