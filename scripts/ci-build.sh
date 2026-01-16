0f0a07fcaaee7dded0655745c2061501bbee0529
celenity <celenity@celenity.dev>
2026-01-15 20:58:47 -0500

    fix: source env_local after running prebuild.sh
    Signed-off-by: celenity <celenity@celenity.dev>

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -42,10 +42,10 @@ fi
 # Get sources
 bash -x ./scripts/get_sources.sh
 
-source "$(realpath $(dirname "$0"))/env_local.sh"
-
 # Prepare sources
 bash -x ./scripts/prebuild.sh "${BUILD_VARIANT}"
+
+source "$(realpath $(dirname "$0"))/env_local.sh"
 
 if [[ "$BUILD_TYPE" == "bundle" ]]; then
     export MOZ_ANDROID_FAT_AAR_ARCHITECTURES='arm64-v8a,armeabi-v7a,x86_64'
