30d59c92331a137cb7faf390e31bb5734e57ece9
celenity <celenity@celenity.dev>
2026-02-11 18:33:21 -0500

    fix: ensure that `sign.sh` variables are formatted/used correctly
    Signed-off-by: celenity <celenity@celenity.dev>

--- scripts/sign.sh
+++ scripts/sign.sh
@@ -14,39 +14,39 @@ source "${IRONFOX_VERSIONS}"
 
 function sign_apk() {
     target="$1"
-    APK_IN="${IRONFOX_OUTPUTS_APK}/ironfox-${IRONFOX_CHANNEL}-${target}-unsigned.apk"
+    APK_IN=${IRONFOX_OUTPUTS_APK}/ironfox-${IRONFOX_CHANNEL}-${target}-unsigned.apk
 
     if [ "${IRONFOX_CI}" == 1 ]; then
-        APK_OUT="${IRONFOX_APK_ARTIFACTS}/IronFox-v${IRONFOX_VERSION}-${target}.apk"
+        APK_OUT=${IRONFOX_APK_ARTIFACTS}/IronFox-v${IRONFOX_VERSION}-${target}.apk
     else
-        APK_OUT="${IRONFOX_OUTPUTS_APK}/ironfox-${IRONFOX_CHANNEL}-${target}-signed.apk"
+        APK_OUT=${IRONFOX_OUTPUTS_APK}/ironfox-${IRONFOX_CHANNEL}-${target}-signed.apk
     fi
 
     "${IRONFOX_ANDROID_SDK}/build-tools/${ANDROID_BUILDTOOLS_VERSION}/apksigner" sign \
-      --ks="${IRONFOX_KEYSTORE}" \
-      --ks-pass="file:/${IRONFOX_KEYSTORE_PASS_FILE}" \
-      --ks-key-alias="${IRONFOX_KEYSTORE_KEY_ALIAS}" \
-      --key-pass="file:/${IRONFOX_KEYSTORE_KEY_PASS_FILE}" \
-      --out="${APK_OUT}" \
+      --ks=${IRONFOX_KEYSTORE} \
+      --ks-pass=file:/${IRONFOX_KEYSTORE_PASS_FILE} \
+      --ks-key-alias=${IRONFOX_KEYSTORE_KEY_ALIAS} \
+      --key-pass=file:/${IRONFOX_KEYSTORE_KEY_PASS_FILE} \
+      --out=${APK_OUT} \
     "${APK_IN}"
 }
 
 function sign_bundle() {
-    AAB_IN="${IRONFOX_OUTPUTS_FENIX_AAB}"
+    AAB_IN=${IRONFOX_OUTPUTS_FENIX_AAB}
 
     if [ "${IRONFOX_CI}" == 1 ]; then
-        APKS_OUT="${IRONFOX_APKS_ARTIFACTS}/IronFox-v${IRONFOX_VERSION}.apks"
+        APKS_OUT=${IRONFOX_APKS_ARTIFACTS}/IronFox-v${IRONFOX_VERSION}.apks
     else
-        APKS_OUT="${IRONFOX_OUTPUTS_FENIX_APKS}"
+        APKS_OUT=${IRONFOX_OUTPUTS_FENIX_APKS}
     fi
 
     "${IRONFOX_BUNDLETOOL}" build-apks \
-      --bundle="${AAB_IN}" \
-      --output="${APKS_OUT}" \
-      --ks="${IRONFOX_KEYSTORE}" \
-      --ks-pass="file:/${IRONFOX_KEYSTORE_PASS_FILE}" \
-      --ks-key-alias="${IRONFOX_KEYSTORE_KEY_ALIAS}" \
-      --key-pass="file:/${IRONFOX_KEYSTORE_KEY_PASS_FILE}"
+      --bundle=${AAB_IN} \
+      --output=${APKS_OUT} \
+      --ks=${IRONFOX_KEYSTORE} \
+      --ks-pass=file:/${IRONFOX_KEYSTORE_PASS_FILE} \
+      --ks-key-alias=${IRONFOX_KEYSTORE_KEY_ALIAS} \
+      --key-pass=file:/${IRONFOX_KEYSTORE_KEY_PASS_FILE}
 }
 
 if [ "${IRONFOX_TARGET_ARCH}" == 'bundle' ]; then
