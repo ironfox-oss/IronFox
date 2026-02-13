f4ebb8f37ab7e47d36b301ca820af7b3a12e18f6
celenity <celenity@celenity.dev>
2026-02-13 18:51:35 +0000

    fix: update names of key files for clarity/consistency
    Signed-off-by: celenity <celenity@celenity.dev>

--- scripts/env_ci.sh
+++ scripts/env_ci.sh
@@ -1,14 +1,14 @@
 # IronFox CI environment variables
 
 # Android keystore/app signing
-export IRONFOX_KEYSTORE='/opt/IronFox/signing-key.jks'
+export IRONFOX_KEYSTORE='/opt/IronFox/ironfox-keystore.jks'
 export IRONFOX_KEYSTORE_KEY_ALIAS='ironfox'
-export IRONFOX_KEYSTORE_KEY_PASS_FILE='/opt/IronFox/signing-key.pass'
-export IRONFOX_KEYSTORE_PASS_FILE='/opt/IronFox/keystore.pass'
+export IRONFOX_KEYSTORE_KEY_PASS_FILE='/opt/IronFox/ironfox-signing-key.pass'
+export IRONFOX_KEYSTORE_PASS_FILE='/opt/IronFox/ironfox-keystore.pass'
 
 # Build date
 export IF_BUILD_DATE="${CI_PIPELINE_CREATED_AT}"
 export MOZ_BUILD_DATE="$("${IRONFOX_DATE}" -d "${CI_PIPELINE_CREATED_AT}" "+%Y%m%d%H%M%S")"
 
 # Safe Browsing
-export IRONFOX_SB_GAPI_KEY_FILE='/opt/IronFox/sb-gapi.data'
+export IRONFOX_SB_GAPI_KEY_FILE='/opt/IronFox/ironfox-sb-gapi.data'
