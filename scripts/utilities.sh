e04770d71d9d3279796b7b19c8242e0bf897e9d0
celenity <celenity@celenity.dev>
2026-02-13 20:10:24 -0500

    fix: minor formatting
    Signed-off-by: celenity <celenity@celenity.dev>

--- scripts/utilities.sh
+++ scripts/utilities.sh
@@ -3,10 +3,10 @@
 # Utility functions for frequently performed tasks
 # This file MUST NOT contain anything other than function definitions.
 
-echo_red_text() {
+function echo_red_text() {
 	echo -e "\033[31m$1\033[0m"
 }
 
-echo_green_text() {
+function echo_green_text() {
 	echo -e "\033[32m$1\033[0m"
 }
