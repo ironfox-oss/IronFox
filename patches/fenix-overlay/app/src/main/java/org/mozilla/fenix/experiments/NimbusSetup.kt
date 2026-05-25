8dab081fff5e04fd0a6b90a746c536b771539109
celenity <celenity@celenity.dev>
2026-05-25 01:52:49 +0000

    feat: Override values for settings from Nimbus directly (via our own `initial_experiments.json` file)
    This significantly reduces the time and amount of patches we need, and makes it signiifcantly easier to control/override unwanted behavior
    
    Signed-off-by: celenity <celenity@celenity.dev>

--- patches/fenix-overlay/app/src/main/java/org/mozilla/fenix/experiments/NimbusSetup.kt
+++ patches/fenix-overlay/app/src/main/java/org/mozilla/fenix/experiments/NimbusSetup.kt
@@ -3,26 +3,35 @@
 package org.mozilla.fenix.experiments
 
 import android.content.Context
+import mozilla.appservices.remotesettings.RemoteSettingsService
 import mozilla.components.service.nimbus.NimbusApi
 import mozilla.components.service.nimbus.NimbusAppInfo
 import mozilla.components.service.nimbus.NimbusBuilder
 import org.json.JSONObject
+import org.mozilla.experiments.nimbus.internal.NimbusServerSettings
+import org.mozilla.fenix.R
+import org.mozilla.fenix.ext.settings
 
-fun createNimbus(context: Context, urlString: String?, remoteSettingsService: Any?, geckoPrefHandler: Any?): NimbusApi {
-    val serverSettings = null
+fun createNimbus(context: Context, urlString: String?, remoteSettingsService: RemoteSettingsService?, geckoPrefHandler: Any?): NimbusApi {
+    val serverSettings: NimbusServerSettings? = remoteSettingsService?.let { service ->
+        NimbusServerSettings(
+            rsService = service,
+            collectionName = "noop",
+        )
+    }
 
     val appInfo = NimbusAppInfo(
-        appName = "",
-        channel = "",
+        appName = "fenix",
+        channel = "release",
         customTargetingAttributes = JSONObject(),
     )
 
     return NimbusBuilder(context).apply {
         url = null
         errorReporter = context::reportError
-        initialExperiments = null
+        initialExperiments = R.raw.initial_experiments
         timeoutLoadingExperiment = 200L
-        sharedPreferences = null
+        sharedPreferences = context.settings().preferences
         isFirstRun = true
         featureManifest = null
         onFetchCallback = {}
