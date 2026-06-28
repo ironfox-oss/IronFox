3c5069b078e1b7b3c7d28da1decec05933af3532
celenity <celenity@celenity.dev>
2026-06-28 05:49:45 +0000

    fix: Clean-up wallpaper handling
    Removes IronFoxWallpaperDictionary, and instead uses the strings directly - to reduce complexity and avoid extra overhead
    
    (Also includes some formatting changes)
    
    Signed-off-by: celenity <celenity@celenity.dev>

--- patches/fenix-overlay/app/src/main/java/org/ironfoxoss/ironfox/utils/IronFoxWallpaperUtils.kt
+++ patches/fenix-overlay/app/src/main/java/org/ironfoxoss/ironfox/utils/IronFoxWallpaperUtils.kt
@@ -1,119 +1,119 @@
 package org.ironfoxoss.ironfox.utils
 
 import android.content.res.Configuration
-import org.ironfoxoss.ironfox.utils.IronFoxWallpaperDictionary
+import org.mozilla.fenix.R
 
 // Helpers for managing IronFox wallpapers
 
 object IronFoxWallpaperUtils {
 
-    /**
-     * Get a wallpaper's drawable resource
-     *
-     * @param wallpaperName The name of the wallpaper
-     * @param orientation The desired wallpaper orientation
-     */
-    fun getWallpaperDrawable(
-        wallpaperName: String,
-        orientation: Int
-    ): Int {
-        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            return getWallpaperDrawableLandscape(wallpaperName)
-        } else {
-            return getWallpaperDrawablePortrait(wallpaperName)
-        }
+  /**
+   * Get a wallpaper's drawable resource
+   *
+   * @param wallpaperName The name of the wallpaper
+   * @param orientation The desired wallpaper orientation
+   */
+  fun getWallpaperDrawable(
+    wallpaperName: String,
+    orientation: Int
+  ): Int {
+    if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+      return getWallpaperDrawableLandscape(wallpaperName)
+    } else {
+      return getWallpaperDrawablePortrait(wallpaperName)
     }
+  }
 
-    /**
-     * Get a wallpaper's landscape drawable resource
-     *
-     * @param wallpaperName The name of the wallpaper
-     */
-    internal fun getWallpaperDrawableLandscape(
-        wallpaperName: String
-    ): Int {
-        if (wallpaperName == IronFoxWallpaperDictionary.algaeWallpaperName) {
-            return IronFoxWallpaperDictionary.algaeLandscapeDrawable
-        } else if (wallpaperName == IronFoxWallpaperDictionary.blackWallpaperName) {
-            return IronFoxWallpaperDictionary.blackLandscapeDrawable
-        } else if (wallpaperName == IronFoxWallpaperDictionary.colorfulBubblesWallpaperName) {
-            return IronFoxWallpaperDictionary.colorfulBubblesLandscapeDrawable
-        } else if (wallpaperName == IronFoxWallpaperDictionary.darkDuneWallpaperName) {
-            return IronFoxWallpaperDictionary.darkDuneLandscapeDrawable
-        } else if (wallpaperName == IronFoxWallpaperDictionary.deepBlueWallpaperName) {
-            return IronFoxWallpaperDictionary.deepBlueLandscapeDrawable
-        } else if (wallpaperName == IronFoxWallpaperDictionary.duneWallpaperName) {
-            return IronFoxWallpaperDictionary.duneLandscapeDrawable
-        } else if (wallpaperName == IronFoxWallpaperDictionary.fireyRedWallpaperName) {
-            return IronFoxWallpaperDictionary.fireyRedLandscapeDrawable
-        } else if (wallpaperName == IronFoxWallpaperDictionary.redWallpaperName) {
-            return IronFoxWallpaperDictionary.redLandscapeDrawable
-        } else if (wallpaperName == IronFoxWallpaperDictionary.whiteWallpaperName) {
-            return IronFoxWallpaperDictionary.whiteLandscapeDrawable
-        } else {
-            return IronFoxWallpaperDictionary.pinkLandscapeDrawable
-        }
+  /**
+   * Get a wallpaper's landscape drawable resource
+   *
+   * @param wallpaperName The name of the wallpaper
+   */
+  internal fun getWallpaperDrawableLandscape(
+    wallpaperName: String
+  ): Int {
+    if (wallpaperName == "algae") {
+      return R.drawable.algae_landscape
+    } else if (wallpaperName == "black") {
+      return R.drawable.black_landscape
+    } else if (wallpaperName == "colorful-bubbles") {
+      return R.drawable.colorful_bubbles_landscape
+    } else if (wallpaperName == "dark-dune") {
+      return R.drawable.dark_dune_landscape
+    } else if (wallpaperName == "deep-blue") {
+      return R.drawable.deep_blue_landscape
+    } else if (wallpaperName == "dune") {
+      return R.drawable.dune_landscape
+    } else if (wallpaperName == "firey-red") {
+      return R.drawable.firey_red_landscape
+    } else if (wallpaperName == "red") {
+      return R.drawable.red_landscape
+    } else if (wallpaperName == "white") {
+      return R.drawable.white_landscape
+    } else {
+      return R.drawable.pink_landscape
     }
+  }
 
-    /**
-     * Get a wallpaper's portrait drawable resource
-     *
-     * @param wallpaperName The name of the wallpaper
-     */
-    internal fun getWallpaperDrawablePortrait(
-        wallpaperName: String
-    ): Int {
-        if (wallpaperName == IronFoxWallpaperDictionary.algaeWallpaperName) {
-            return IronFoxWallpaperDictionary.algaePortraitDrawable
-        } else if (wallpaperName == IronFoxWallpaperDictionary.blackWallpaperName) {
-            return IronFoxWallpaperDictionary.blackPortraitDrawable
-        } else if (wallpaperName == IronFoxWallpaperDictionary.colorfulBubblesWallpaperName) {
-            return IronFoxWallpaperDictionary.colorfulBubblesPortraitDrawable
-        } else if (wallpaperName == IronFoxWallpaperDictionary.darkDuneWallpaperName) {
-            return IronFoxWallpaperDictionary.darkDunePortraitDrawable
-        } else if (wallpaperName == IronFoxWallpaperDictionary.deepBlueWallpaperName) {
-            return IronFoxWallpaperDictionary.deepBluePortraitDrawable
-        } else if (wallpaperName == IronFoxWallpaperDictionary.duneWallpaperName) {
-            return IronFoxWallpaperDictionary.dunePortraitDrawable
-        } else if (wallpaperName == IronFoxWallpaperDictionary.fireyRedWallpaperName) {
-            return IronFoxWallpaperDictionary.fireyRedPortraitDrawable
-        } else if (wallpaperName == IronFoxWallpaperDictionary.redWallpaperName) {
-            return IronFoxWallpaperDictionary.redPortraitDrawable
-        } else if (wallpaperName == IronFoxWallpaperDictionary.whiteWallpaperName) {
-            return IronFoxWallpaperDictionary.whitePortraitDrawable
-        } else {
-            return IronFoxWallpaperDictionary.pinkPortraitDrawable
-        }
+  /**
+   * Get a wallpaper's portrait drawable resource
+   *
+   * @param wallpaperName The name of the wallpaper
+   */
+  internal fun getWallpaperDrawablePortrait(
+    wallpaperName: String
+  ): Int {
+    if (wallpaperName == "algae") {
+      return R.drawable.algae_portrait
+    } else if (wallpaperName == "black") {
+      return R.drawable.black_portrait
+    } else if (wallpaperName == "colorful-bubbles") {
+      return R.drawable.colorful_bubbles_portrait
+    } else if (wallpaperName == "dark-dune") {
+      return R.drawable.dark_dune_portrait
+    } else if (wallpaperName == "deep-blue") {
+      return R.drawable.deep_blue_portrait
+    } else if (wallpaperName == "dune") {
+      return R.drawable.dune_portrait
+    } else if (wallpaperName == "firey-red") {
+      return R.drawable.firey_red_portrait
+    } else if (wallpaperName == "red") {
+      return R.drawable.red_portrait
+    } else if (wallpaperName == "white") {
+      return R.drawable.white_portrait
+    } else {
+      return R.drawable.pink_portrait
     }
+  }
 
-    /**
-     * Get a wallpaper's thumbnail drawable resource
-     *
-     * @param wallpaperName The name of the wallpaper
-     */
-    fun getWallpaperDrawableThumbnail(
-        wallpaperName: String
-    ): Int {
-        if (wallpaperName == IronFoxWallpaperDictionary.algaeWallpaperName) {
-            return IronFoxWallpaperDictionary.algaeThumbnailDrawable
-        } else if (wallpaperName == IronFoxWallpaperDictionary.blackWallpaperName) {
-            return IronFoxWallpaperDictionary.blackThumbnailDrawable
-        } else if (wallpaperName == IronFoxWallpaperDictionary.colorfulBubblesWallpaperName) {
-            return IronFoxWallpaperDictionary.colorfulBubblesThumbnailDrawable
-        } else if (wallpaperName == IronFoxWallpaperDictionary.darkDuneWallpaperName) {
-            return IronFoxWallpaperDictionary.darkDuneThumbnailDrawable
-        } else if (wallpaperName == IronFoxWallpaperDictionary.deepBlueWallpaperName) {
-            return IronFoxWallpaperDictionary.deepBlueThumbnailDrawable
-        } else if (wallpaperName == IronFoxWallpaperDictionary.duneWallpaperName) {
-            return IronFoxWallpaperDictionary.duneThumbnailDrawable
-        } else if (wallpaperName == IronFoxWallpaperDictionary.fireyRedWallpaperName) {
-            return IronFoxWallpaperDictionary.fireyRedThumbnailDrawable
-        } else if (wallpaperName == IronFoxWallpaperDictionary.redWallpaperName) {
-            return IronFoxWallpaperDictionary.redThumbnailDrawable
-        } else if (wallpaperName == IronFoxWallpaperDictionary.whiteWallpaperName) {
-            return IronFoxWallpaperDictionary.whiteThumbnailDrawable
-        } else {
-            return IronFoxWallpaperDictionary.pinkThumbnailDrawable
-        }
+  /**
+   * Get a wallpaper's thumbnail drawable resource
+   *
+   * @param wallpaperName The name of the wallpaper
+   */
+  fun getWallpaperDrawableThumbnail(
+    wallpaperName: String
+  ): Int {
+    if (wallpaperName == "algae") {
+      return R.drawable.algae_thumbnail
+    } else if (wallpaperName == "black") {
+      return R.drawable.black_thumbnail
+    } else if (wallpaperName == "colorful-bubbles") {
+      return R.drawable.colorful_bubbles_thumbnail
+    } else if (wallpaperName == "dark-dune") {
+      return R.drawable.dark_dune_thumbnail
+    } else if (wallpaperName == "deep-blue") {
+      return R.drawable.deep_blue_thumbnail
+    } else if (wallpaperName == "dune") {
+      return R.drawable.dune_thumbnail
+    } else if (wallpaperName == "firey-red") {
+      return R.drawable.firey_red_thumbnail
+    } else if (wallpaperName == "red") {
+      return R.drawable.red_thumbnail
+    } else if (wallpaperName == "white") {
+      return R.drawable.white_thumbnail
+    } else {
+      return R.drawable.pink_thumbnail
     }
+  }
 }
