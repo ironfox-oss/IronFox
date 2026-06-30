3ffc8f7c638b60c55b30d89f0b5571b69f6b8a64
celenity <celenity@celenity.dev>
2026-06-30 07:31:07 +0000

    fix: ensure we get/properly use the preference key for hiding/displaying prefs at `FenixSettingsVisibilityManager`
    Signed-off-by: celenity <celenity@celenity.dev>

--- patches/fenix-overlay/app/src/main/java/org/ironfoxoss/ironfox/utils/FenixSettingsVisibilityManager.kt
+++ patches/fenix-overlay/app/src/main/java/org/ironfoxoss/ironfox/utils/FenixSettingsVisibilityManager.kt
@@ -15,67 +15,67 @@ object FenixSettingsVisibilityManager {
   /**
    * Control the visibility of settings at the general settings fragment
    *
-   * @param context The application context
-   * @param prefFragment The preference fragment from where the preference should be displayed
+   * @param context Application context
+   * @param prefFragment Preference fragment
    */
   fun SettingsFragment(
     context: Context,
     prefFragment: PreferenceFragmentCompat
   ) {
-    hidePreference(R.string.pref_key_data_choices, prefFragment)
-    hidePreference(R.string.pref_key_link_sharing, prefFragment)
-    hidePreference(R.string.pref_key_nimbus_experiments, prefFragment)
-    hidePreference(R.string.pref_key_remote_improvements, prefFragment)
-    hidePreference(R.string.pref_key_rate, prefFragment)
-    hidePreference(R.string.pref_key_start_profiler, prefFragment)
-    hidePreference(R.string.pref_key_remote_debugging, prefFragment)
+    hidePreference(context, R.string.pref_key_data_choices, prefFragment)
+    hidePreference(context, R.string.pref_key_link_sharing, prefFragment)
+    hidePreference(context, R.string.pref_key_nimbus_experiments, prefFragment)
+    hidePreference(context, R.string.pref_key_remote_improvements, prefFragment)
+    hidePreference(context, R.string.pref_key_rate, prefFragment)
+    hidePreference(context, R.string.pref_key_start_profiler, prefFragment)
+    hidePreference(context, R.string.pref_key_remote_debugging, prefFragment)
     hideLocalAddonInstall(context, prefFragment)
   }
 
   /**
    * Control the visibility of settings at the secret settings fragment
    *
-   * @param context The application context
-   * @param prefFragment The preference fragment from where the preference should be displayed
+   * @param context Application context
+   * @param prefFragment Preference fragment
    */
   fun SecretSettingsFragment(
     context: Context,
     prefFragment: PreferenceFragmentCompat
   ) {
-    displayPreference(R.string.pref_key_allow_settings_search, prefFragment)
-    displayPreference(R.string.pref_key_enable_address_sync, prefFragment)
-    displayPreference(R.string.pref_key_enable_homepage_as_new_tab, prefFragment)
-    displayPreference(R.string.pref_key_enable_import_bookmarks, prefFragment)
-    displayPreference(R.string.pref_key_enable_ip_protection, prefFragment)
-    displayPreference(R.string.pref_key_enable_isolated_process, prefFragment)
-    displayPreference(R.string.pref_key_enable_lna_blocking_enabled, prefFragment)
-    displayPreference(R.string.pref_key_enable_lna_feature_enabled, prefFragment)
-    displayPreference(R.string.pref_key_enable_lna_tracker_blocking_enabled, prefFragment)
-    displayPreference(R.string.pref_key_enable_longfox, prefFragment)
-    displayPreference(R.string.pref_key_native_share_sheet, prefFragment)
-    displayPreference(R.string.pref_key_should_show_custom_tab_extensions, prefFragment)
-    displayPreference(R.string.pref_key_tab_groups, prefFragment)
-    displayPreference(R.string.pref_key_tab_groups_drag_and_drop, prefFragment)
-    displayPreference(R.string.pref_key_tracking_protection_dashboard_status, prefFragment)
-    displayPreference(R.string.pref_key_use_minimal_bottom_toolbar_while_entering_text, prefFragment)
-    displayPreference(R.string.pref_key_use_scroll_data_for_dynamic_toolbar, prefFragment)
+    displayPreference(context, R.string.pref_key_allow_settings_search, prefFragment)
+    displayPreference(context, R.string.pref_key_enable_address_sync, prefFragment)
+    displayPreference(context, R.string.pref_key_enable_homepage_as_new_tab, prefFragment)
+    displayPreference(context, R.string.pref_key_enable_import_bookmarks, prefFragment)
+    displayPreference(context, R.string.pref_key_enable_ip_protection, prefFragment)
+    displayPreference(context, R.string.pref_key_enable_isolated_process, prefFragment)
+    displayPreference(context, R.string.pref_key_enable_lna_blocking_enabled, prefFragment)
+    displayPreference(context, R.string.pref_key_enable_lna_feature_enabled, prefFragment)
+    displayPreference(context, R.string.pref_key_enable_lna_tracker_blocking_enabled, prefFragment)
+    displayPreference(context, R.string.pref_key_enable_longfox, prefFragment)
+    displayPreference(context, R.string.pref_key_native_share_sheet, prefFragment)
+    displayPreference(context, R.string.pref_key_should_show_custom_tab_extensions, prefFragment)
+    displayPreference(context, R.string.pref_key_tab_groups, prefFragment)
+    displayPreference(context, R.string.pref_key_tab_groups_drag_and_drop, prefFragment)
+    displayPreference(context, R.string.pref_key_tracking_protection_dashboard_status, prefFragment)
+    displayPreference(context, R.string.pref_key_use_minimal_bottom_toolbar_while_entering_text, prefFragment)
+    displayPreference(context, R.string.pref_key_use_scroll_data_for_dynamic_toolbar, prefFragment)
     displayAppZygote(context, prefFragment)
 
-    hidePreference(R.string.pref_key_crash_pull_never_show_again, prefFragment)
-    hidePreference(R.string.pref_key_enable_homepage_sports_widget, prefFragment)
-    hidePreference(R.string.pref_key_enable_mozilla_ads_client, prefFragment)
-    hidePreference(R.string.pref_key_microsurvey_feature_enabled, prefFragment)
-    hidePreference(R.string.pref_key_nimbus_use_preview, prefFragment)
-    hidePreference(R.string.pref_key_private_mode_and_stories_entry_point, prefFragment)
-    hidePreference(R.string.pref_key_use_new_crash_reporter, prefFragment)
-    hidePreference(R.string.pref_key_use_remote_search_configuration, prefFragment)
+    hidePreference(context, R.string.pref_key_crash_pull_never_show_again, prefFragment)
+    hidePreference(context, R.string.pref_key_enable_homepage_sports_widget, prefFragment)
+    hidePreference(context, R.string.pref_key_enable_mozilla_ads_client, prefFragment)
+    hidePreference(context, R.string.pref_key_microsurvey_feature_enabled, prefFragment)
+    hidePreference(context, R.string.pref_key_nimbus_use_preview, prefFragment)
+    hidePreference(context, R.string.pref_key_private_mode_and_stories_entry_point, prefFragment)
+    hidePreference(context, R.string.pref_key_use_new_crash_reporter, prefFragment)
+    hidePreference(context, R.string.pref_key_use_remote_search_configuration, prefFragment)
   }
 
   /**
    * Control the visibility of settings at the site settings fragment
    *
-   * @param context The application context
-   * @param prefFragment The preference fragment from where the preference should be displayed
+   * @param context Application context
+   * @param prefFragment Preference fragment
    */
   fun SiteSettingsFragment(
     context: Context,
@@ -87,25 +87,25 @@ object FenixSettingsVisibilityManager {
   /**
    * Control the visibility of settings at the tracking protection fragment
    *
-   * @param context The application context
-   * @param prefFragment The preference fragment from where the preference should be displayed
+   * @param context Application context
+   * @param prefFragment Preference fragment
    */
   fun TrackingProtectionFragment(
     context: Context,
     prefFragment: PreferenceFragmentCompat
   ) {
-    hidePreference(R.string.pref_key_privacy_enable_global_privacy_control, prefFragment)
-    hidePreference(R.string.pref_key_tracking_protection, prefFragment)
-    hidePreference(R.string.pref_key_tracking_protection_custom_option, prefFragment)
-    hidePreference(R.string.pref_key_tracking_protection_standard_option, prefFragment)
-    hidePreference(R.string.pref_key_tracking_protection_strict_default, prefFragment)
+    hidePreference(context, R.string.pref_key_privacy_enable_global_privacy_control, prefFragment)
+    hidePreference(context, R.string.pref_key_tracking_protection, prefFragment)
+    hidePreference(context, R.string.pref_key_tracking_protection_custom_option, prefFragment)
+    hidePreference(context, R.string.pref_key_tracking_protection_standard_option, prefFragment)
+    hidePreference(context, R.string.pref_key_tracking_protection_strict_default, prefFragment)
   }
 
   /**
    * Display the setting to toggle app zygote preloading
    *
-   * @param context The application context
-   * @param prefFragment The preference fragment from where the preference should be displayed
+   * @param context Application context
+   * @param prefFragment Preference fragment
    */
   internal fun displayAppZygote(
     context: Context,
@@ -119,8 +119,8 @@ object FenixSettingsVisibilityManager {
   /**
    * Hide the EME site setting
    *
-   * @param context The application context
-   * @param prefFragment The preference fragment from where the preference should be displayed
+   * @param context Application context
+   * @param prefFragment Preference fragment
    */
   internal fun hideEme(
     context: Context,
@@ -134,8 +134,8 @@ object FenixSettingsVisibilityManager {
   /**
    * Hide the local add-on installation option
    *
-   * @param context The application context
-   * @param prefFragment The preference fragment from where the preference should be displayed
+   * @param context Application context
+   * @param prefFragment Preference fragment
    */
   internal fun hideLocalAddonInstall(
     context: Context,
@@ -149,13 +149,16 @@ object FenixSettingsVisibilityManager {
   /**
    * Display a hidden UI setting
    *
-   * @param prefKey The preference to display
-   * @param prefFragment The preference fragment from where the preference should be displayed
+   * @param context Application context
+   * @param pref Preference to display
+   * @param prefFragment Preference fragment
    */
   internal fun displayPreference(
-    prefKey: Int,
+    context: Context,
+    pref: Int,
     prefFragment: PreferenceFragmentCompat,
   ) {
+    val prefKey = context.getPreferenceKey(pref)
     val preference = prefFragment.findPreference<Preference>(prefKey)
     preference?.isVisible = true
   }
@@ -163,13 +166,16 @@ object FenixSettingsVisibilityManager {
   /**
    * Hide an unwanted UI setting
    *
-   * @param prefKey The preference to remove
-   * @param prefFragment The preference fragment from where the preference should be removed
+   * @param context Application context
+   * @param pref Preference to remove
+   * @param prefFragment Preference fragment
    */
   internal fun hidePreference(
-    prefKey: Int,
+    context: Context,
+    pref: Int,
     prefFragment: PreferenceFragmentCompat,
   ) {
+    val prefKey = context.getPreferenceKey(pref)
     val preference = prefFragment.findPreference<Preference>(prefKey)
     preference?.isVisible = false
   }
